a3eeda807f30a07b4051d9234dbade92aa731a0f
Doug Shawhan <doug@nsixty.com>
2015-06-06 01:18:01 -0400

    added a spinner. I'm so very.

--- gysin/static/js/gysin.js
+++ gysin/static/js/gysin.js
@@ -9,12 +9,16 @@ function fillChainSpace () {
 		chainverse.push(line);
 	}
 
+	$("#chainwrap").hide();
+	$("#spinnerspace").show();
 	$.get("api/chain", function(data) {
 		$.each(data, function(idx, list){
 			line = list.join(" ");
 			chainverse.push(line);
 		});
 		$("#chainspace").val(chainverse.join("\n"));
+		$("#chainwrap").show();
+		$("#spinnerspace").hide();
 	});
 };
 
@@ -125,5 +129,33 @@ $(document).ready(function() {
 		saveSongSpace();
 		return false;
 	});	
+
+	// start the spinner a spinnin'
+	var opts = {
+		lines: 13 // The number of lines to draw
+		, length: 4 // The length of each line
+		, width: 5 // The line thickness
+		, radius: 21 // The radius of the inner circle
+		, scale: 1 // Scales overall size of the spinner
+		, corners: 0.4 // Corner roundness (0..1)
+		, color: '#000' // #rgb or #rrggbb or array of colors
+		, opacity: 0.2 // Opacity of the lines
+		, rotate: 0 // The rotation offset
+		, direction: -1 // 1: clockwise, -1: counterclockwise
+		, speed: 1.5 // Rounds per second
+		, trail: 55 // Afterglow percentage
+		// Frames per second when using setTimeout()
+		//as a fallback for CSS
+		, fps: 20 
+		, zIndex: 2e9 // The z-index (defaults to 2000000000)
+		, className: 'spinner' // The CSS class to assign to the spinner
+		, top: '35%' // Top position relative to parent
+		, left: '35%' // Left position relative to parent
+		, shadow: true // Whether to render a shadow
+		, hwaccel: true // Whether to use hardware acceleration
+		, position: 'relative' // Element positioning
+	}
+	var target = document.getElementById("spinnerspace");
+	var spinner = new Spinner(opts).spin(target);
 
 });
